95a1c17e608e24f2c44f5ddf5ddfc9201caad4ec
Jack <3jackattack3@gmail.com>
2018-02-12 19:42:06 -0500

    created test class

---
+++ src/Blink.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+class Blink
+{
+  public:
+    Blink(int pin);
+    void flash();
+  private:
+    int _pin;
+};
+
+Blink::Blink(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+void Blink::flash()
+{
+  digitalWrite(_pin, HIGH);
+  delay(500);
+  digitalWrite(_pin, LOW);
+  delay(500);
+}
